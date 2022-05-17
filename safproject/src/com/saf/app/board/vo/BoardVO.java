c326995622685920be2bdfbdbc3e8c359c378106
Jiyeon1104 <2018110325@dgu.ac.kr>
2022-05-18 04:00:46 +0900

    220518

--- safproject/src/com/saf/app/board/vo/BoardVO.java
+++ safproject/src/com/saf/app/board/vo/BoardVO.java
@@ -1,60 +1,61 @@
 package com.saf.app.board.vo;
 
 public class BoardVO {
-	private int boardNumber;
-	private String boardTitle;
-	private String boardContent;
-	private int memberNumber;
-	private String boardDate;
-	private int readCount;
+	private int bnumber;
+	private String btitle;
+	private String bcontent;
+	private String bdate;
+	private int unum;
+	private String bimage;
+	public int getBnumber() {
+		return bnumber;
+	}
+	public void setBnumber(int bnumber) {
+		this.bnumber = bnumber;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public String getBdate() {
+		return bdate;
+	}
+	public void setBdate(String bdate) {
+		this.bdate = bdate;
+	}
+	public int getUnum() {
+		return unum;
+	}
+	public void setUnum(int unum) {
+		this.unum = unum;
+	}
+	public String getBimage() {
+		return bimage;
+	}
+	public void setBimage(String bimage) {
+		this.bimage = bimage;
+	}
 	
-	public BoardVO() {;}
-
-	public int getBoardNumber() {
-		return boardNumber;
+	public BoardVO() {
+		
 	}
-
-	public void setBoardNumber(int boardNumber) {
-		this.boardNumber = boardNumber;
+	
+	public BoardVO(int bnumber, String btitle, String bcontent, String bdate, int unum, String bimage) {
+		super();
+		this.bnumber = bnumber;
+		this.btitle = btitle;
+		this.bcontent = bcontent;
+		this.bdate = bdate;
+		this.unum = unum;
+		this.bimage = bimage;
 	}
-
-	public String getBoardTitle() {
-		return boardTitle;
-	}
-
-	public void setBoardTitle(String boardTitle) {
-		this.boardTitle = boardTitle;
-	}
-
-	public String getBoardContent() {
-		return boardContent;
-	}
-
-	public void setBoardContent(String boardContent) {
-		this.boardContent = boardContent;
-	}
-
-	public int getMemberNumber() {
-		return memberNumber;
-	}
-
-	public void setMemberNumber(int memberNumber) {
-		this.memberNumber = memberNumber;
-	}
-
-	public String getBoardDate() {
-		return boardDate;
-	}
-
-	public void setBoardDate(String boardDate) {
-		this.boardDate = boardDate;
-	}
-
-	public int getReadCount() {
-		return readCount;
-	}
-
-	public void setReadCount(int readCount) {
-		this.readCount = readCount;
-	}
+	
 }
